69fdbc93ab7df7f58a74456d7e677afbe752ec36
Ikem Nkurumeh <ikemnkur@gmail.com>
2024-05-15 01:53:25 -0500

    modds ignore

--- uplogic-new.py
+++ uplogic-new.py
@@ -31,7 +31,33 @@ class UI(bge.types.KX_PythonComponent):
             valign='center',
             bg_color=(1, 1, 1, 1)
         )
+        
+        # This is the triangle marker above the moving Suzanne
+        self.UI_marker = Image(
+            relative={'pos': True},
+            size=[40, 40],
+            texture='UIMarker.png',
+            halign='center',
+            valign='center'
+        )
+
+        # This is the button on the static Suzanne
+        self.suzanne_button = LabelButton(
+            relative={'pos': True},
+            size=[100, 100],
+            halign='center',
+            valign='top',
+            bg_color=(1, .5, 0, .5),
+            text='Click Me',
+            hover_color=[1, .5, 0, 1]
+        )
+        # Defining an action for the button
+        self.suzanne_button.on_click = self.rotate_suzanne
+        
+        # Order is important for drawing - last object is always on top
         self.floating_ui.add_widget(self.reticle)
+        self.floating_ui.add_widget(self.UI_marker)
+        self.floating_ui.add_widget(self.suzanne_button)
 
         # Build Menu UI
         self.menu = Canvas()
@@ -45,6 +71,7 @@ class UI(bge.types.KX_PythonComponent):
             border_color=[1, 1, 1, 1]
         )
 
+        # Left Menu
         self.left_menu = BoxLayout(
             relative={'pos': True, 'size': True},
             pos=[0, 0],
@@ -55,6 +82,7 @@ class UI(bge.types.KX_PythonComponent):
             orientation='vertical'
         )
 
+        # Return to game button
         self.return_button = LabelButton(
             relative={'size': True, 'pos': True},
             pos=[1, 1],
@@ -66,6 +94,7 @@ class UI(bge.types.KX_PythonComponent):
         )
         self.return_button.on_click = self.set_menu_false
 
+        # Quit game button
         self.quit_button = LabelButton(
             relative={'size': True, 'pos': True},
             pos=[1, 1],
@@ -77,6 +106,7 @@ class UI(bge.types.KX_PythonComponent):
         )
         self.quit_button.on_click = self.quit
 
+        # Info button
         self.info_button = LabelButton(
             relative={'size': True, 'pos': True},
             pos=[1, 1],
@@ -88,6 +118,7 @@ class UI(bge.types.KX_PythonComponent):
         )
         self.info_button.on_click = self.showInfo
 
+        # Right window content
         self.right_window = BoxLayout(
             relative={'pos': True, 'size': True},
             pos=[.2, 0],
@@ -98,6 +129,7 @@ class UI(bge.types.KX_PythonComponent):
             orientation='vertical'
         )
 
+        # Text layout for displaying info
         self.right_textLayout = BoxLayout(
             relative={'pos': True, 'size': True},
             pos=[.5, .4],
@@ -108,6 +140,7 @@ class UI(bge.types.KX_PythonComponent):
             orientation='vertical'
         )
         
+        # Info text input
         self.info_text = TextInput(
             relative={'pos': False},
             pos=[.5, .1],
@@ -118,6 +151,7 @@ class UI(bge.types.KX_PythonComponent):
             text='Search'
         )
         
+        # Main text input for displaying object info
         self.main_text = TextInput(
             relative={'pos': True},
             pos=[.5, .5],
@@ -128,6 +162,7 @@ class UI(bge.types.KX_PythonComponent):
             text='Info'
         )
 
+        # Exit menu button
         self.x_button = LabelButton(
             relative={'pos': True},
             pos=[1, 1],
@@ -141,6 +176,7 @@ class UI(bge.types.KX_PythonComponent):
         )
         self.x_button.on_click = self.set_menu_false
         
+        # Add all widgets to the menu
         self.menu.show = False
         self.menu.add_widget(self.frame)
         self.frame.add_widget(self.left_menu)
@@ -155,10 +191,14 @@ class UI(bge.types.KX_PythonComponent):
         self.cursor = Cursor(size=[30, 30], texture='cursor.png')
         self.cursor.show = False
 
+        # Store information about objects
         self.objects_info = self.store_objects_info()
+
+        # Add UI elements for each game object
         self.add_game_objects_ui()
 
     def add_game_objects_ui(self):
+        """Add UI elements for each game object"""
         objects = bge.logic.getCurrentScene().objects
         for obj in objects:
             object_layout = BoxLayout(
@@ -209,50 +249,83 @@ class UI(bge.types.KX_PythonComponent):
             self.right_window.add_widget(object_layout)
 
     def display_object_info(self, obj):
+        """Display the selected object's info in a new layout, hiding the object names layout"""
         obj_info = self.get_object_info(obj)
-        display_text = "\n".join([f"{key}: {value}" for key, value in obj_info.items()])
-        
-        # Create the new overlay layout
+
+        # Hide the object names layout
+        self.right_window.show = False
+
+        # Create the new layout for object info
         self.info_layout = BoxLayout(
             relative={'pos': True, 'size': True},
             pos=[0.1, 0.1],
             size=[0.8, 0.8],
-            bg_color=[0, 0, 0, 0.8],
-            border_width=1,
-            border_color=[1, 1, 1, 1],
+            bg_color=[0, 0, 0, 0],
             orientation='vertical'
         )
 
-        exit_button = LabelButton(
+        # Back button to return to the object names layout
+        back_button = LabelButton(
             relative={'size': True, 'pos': True},
-            pos=[0.95, 0.05],
-            size=[0.05, 0.05],
+            pos=[0.9, 0.9],
+            size=[0.1, 0.1],
             bg_color=[1, 0, 0, 1],
             border_width=1,
             border_color=[1, 1, 1, 1],
-            text='X'
+            text='<-'
         )
-        exit_button.on_click = self.remove_info_layout
+        back_button.on_click = self.show_object_names_layout
 
-        info_text = TextInput(
-            relative={'pos': True},
-            pos=[0.1, 0.2],
-            size=[0.8, 0.6],
-            font_color=[1, 1, 1, 1],
-            font_size=14,
-            shadow=True,
-            shadow_color=[0, 0.1, 0.1, 1],
-            text=display_text
-        )
+        # Display object properties and attributes as LabelButtons with "Change" button
+        for key, value in obj_info.items():
+            property_layout = BoxLayout(
+                relative={'pos': True, 'size': True},
+                pos=[0.9, 0.9],
+                size=[1, .05],
+                bg_color=[0, 0, 0, 0],
+                orientation='horizontal'
+            )
 
-        self.info_layout.add_widget(exit_button)
-        self.info_layout.add_widget(info_text)
+            property_label = LabelButton(
+                relative={'size': True, 'pos': True},
+                pos=[0.2, 0],
+                size=[0.9, 1],
+                bg_color=[0, 0, 0, 0],
+                border_width=1,
+                border_color=[1, 1, 1, 1],
+                text=f"{key}: {value}"
+            )
+
+            change_button = LabelButton(
+                relative={'size': True, 'pos': True},
+                pos=[1, 0],
+                size=[.1, 1],
+                bg_color=[0, 0, 0, 0],
+                border_width=1,
+                border_color=[1, 1, 1, 1],
+                text='Change'
+            )
+            change_button.on_click = lambda *a, key=key: self.change_property(obj, key)
+
+            property_layout.add_widget(property_label)
+            property_layout.add_widget(change_button)
+
+            self.info_layout.add_widget(property_layout)
+
+        self.info_layout.add_widget(back_button)
         self.menu.add_widget(self.info_layout)
 
-    def remove_info_layout(self, *a):
+    def show_object_names_layout(self, *a):
+        """Show the object names layout and hide the object info layout"""
         self.menu.remove_widget(self.info_layout)
+        self.right_window.show = True
+
+    def change_property(self, obj, key):
+        """Handler for the 'Change' button to modify object properties"""
+        print(f"Change property '{key}' of object '{obj.name}'")
 
     def get_object_info(self, obj):
+        """Retrieve information about the selected object"""
         obj_info = {
             'name': obj.name,
             'global_position': list(obj.worldPosition),
@@ -265,6 +338,7 @@ class UI(bge.types.KX_PythonComponent):
             'materials': [mat.name for mat in obj.meshes[0].materials] if obj.meshes else [],
         }
 
+        # Additional attributes for dynamic objects
         if obj.getPhysicsId():
             obj_info.update({
                 'mass': obj.mass,
@@ -274,20 +348,26 @@ class UI(bge.types.KX_PythonComponent):
         return obj_info
 
     def quit(self, *a):
+        """End the game"""
         bge.logic.endGame()
     
     def showInfo(self, *a):
+        """Show information button handler"""
         self.info_button.text = "Working"
         self.store_objects_info()
 
     def edit_object(self, *a):
+        """Edit button handler"""
         print("Edit button clicked")
 
     def toggle_visibility(self, obj, button, *a):
+        """Toggle the visibility of the selected object"""
         obj.visible = not obj.visible
         button.text = 'Show' if obj.visible else 'Hide'
 
     def update(self):
+        """Update method called every frame"""
+        suzanne = self.object.scene.objects['Suzanne']
         if key_press('esc'):
             self.show_menu = not self.show_menu
             self.floating_ui.show = not self.show_menu
@@ -296,7 +376,14 @@ class UI(bge.types.KX_PythonComponent):
             self.cursor.show = self.show_menu
         if self.show_menu:
             pass
+        else:
+            self.UI_marker.pos = Vector(world_to_screen(suzanne)) + Vector((0, .1))
+            self.suzanne_button.pos = world_to_screen(self.object.scene.objects['Suzanne.001'])
+        
+        suzanne.applyRotation((0, 0, -.01))
+        suzanne.applyMovement((.1, 0, 0), True)
 
+        # Update displayed object info periodically
         if hasattr(self, 'update_counter'):
             self.update_counter -= 1
             if self.update_counter <= 0:
@@ -306,7 +393,12 @@ class UI(bge.types.KX_PythonComponent):
         else:
             self.update_counter = 12
 
+    def rotate_suzanne(self, *a):
+        self.object.scene.objects['Suzanne.001'].applyRotation((0, 0, 3.1516 * .25))
+
+
     def set_menu_false(self, arg):
+        """Set the menu visibility to false"""
         self.show_menu = False
         self.floating_ui.show = not self.show_menu
         self.mouselook.active = not self.show_menu
@@ -314,6 +406,7 @@ class UI(bge.types.KX_PythonComponent):
         self.cursor.show = self.show_menu
 
     def store_objects_info(self):
+        """Store information about all objects in the scene"""
         scene = bge.logic.getCurrentScene()
         objects_info = []
 
